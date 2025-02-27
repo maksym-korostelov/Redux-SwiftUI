312f855750d7c4474cf408abd2dc9b0166700510
Maksym Korostelov <Maksym_Korostelov@epam.com>
2025-02-27 19:42:39 +0100

    initial commit

---
+++ Redux-SwiftUI/Redux_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Redux_SwiftUIApp.swift
+//  Redux-SwiftUI
+//
+//  Created by Maksym Korostelov on 27/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Redux_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
